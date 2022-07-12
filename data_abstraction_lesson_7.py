6f69cba692a1576225f997817dff97c1e3cf3172
AntonVorontsov <vorontsovanton@gmail.com>
2022-07-12 13:48:05 +0300

    first commit for the Hexlet tests

--- data_abstraction_lesson_7.py
+++ data_abstraction_lesson_7.py
@@ -1,31 +1,7 @@
-def make_decart_point(x, y):
-    return {"x": x, "y": y}
+from points import get_quadrant, get_x, get_y, make_decart_point
 
 
-def get_x(point):
-    return point["x"]
-
-
-def get_y(point):
-    return point["y"]
-
-
-def get_quadrant(point):
-    x = get_x(point)
-    y = get_y(point)
-
-    if x > 0 and y > 0:
-        return 1
-    if x < 0 < y:
-        return 2
-    if x < 0 and y < 0:
-        return 3
-    if y < 0 < x:
-        return 4
-
-    return None
-
-
+# BEGIN (write your solution here)
 def make_rectangle(start_point, width, height):
     return {
         "start_point": start_point,
@@ -76,10 +52,4 @@ def contains_origin(rectangle):
     )
     return len(set(points_quadrants)) == 4
 
-
-p = make_decart_point(-4, 3)
-rectangle1 = make_rectangle(p, 5, 4)
-print(contains_origin(rectangle1))
-
-rectangle2 = make_rectangle(make_decart_point(-4, 3), 5, 4)
-print(contains_origin(rectangle2))
+# END
